d9d9892c084e5a825aa321e5aa428387608b2af1
Brandon Donnelson <branflake2267@gmail.com>
2013-06-05 20:10:16 -0700

    Can't seem to get it to work

--- GWTPold/src/main/java/com/test/client/gin/ClientModule.java
+++ GWTPold/src/main/java/com/test/client/gin/ClientModule.java
@@ -1,27 +1,32 @@
 package com.test.client.gin;
 
+import com.gwtplatform.mvp.client.annotations.DefaultPlace;
+import com.gwtplatform.mvp.client.annotations.ErrorPlace;
+import com.gwtplatform.mvp.client.annotations.UnauthorizedPlace;
 import com.gwtplatform.mvp.client.gin.AbstractPresenterModule;
 import com.gwtplatform.mvp.client.gin.DefaultModule;
-import com.test.client.place.ClientPlaceManager;
 import com.test.client.core.MainPagePresenter;
 import com.test.client.core.MainPageView;
-import com.test.client.place.DefaultPlace;
-import com.test.client.place.NameTokens;
 import com.test.client.core.ResponsePresenter;
 import com.test.client.core.ResponseView;
+import com.test.client.place.NameTokens;
 
 public class ClientModule extends AbstractPresenterModule {
 
     @Override
     protected void configure() {
-        install(new DefaultModule(ClientPlaceManager.class));
+        install(new DefaultModule());
+
+        // DefaultPlaceManager Places
+        bindConstant().annotatedWith(DefaultPlace.class).to(NameTokens.main);
+        bindConstant().annotatedWith(ErrorPlace.class).to(NameTokens.main);
+        bindConstant().annotatedWith(UnauthorizedPlace.class).to(NameTokens.main);
 
         bindPresenter(MainPagePresenter.class, MainPagePresenter.MyView.class, MainPageView.class,
                 MainPagePresenter.MyProxy.class);
 
-        bindConstant().annotatedWith(DefaultPlace.class).to(NameTokens.main);
-
         bindPresenter(ResponsePresenter.class, ResponsePresenter.MyView.class, ResponseView.class,
                 ResponsePresenter.MyProxy.class);
+
     }
 }
